5af8a0b7ad5477afc1c4d842f1b2aa4339c698b2
amercader <amercadero@gmail.com>
2022-03-11 13:29:54 +0100

    Add some more CLI tests

--- ckanext/sitesearch/tests/test_cli.py
+++ ckanext/sitesearch/tests/test_cli.py
@@ -8,6 +8,70 @@ from ckan.cli.cli import ckan
 
 @pytest.mark.usefixtures("clean_db", "clean_index")
 class TestSiteSearchCLI:
+    def test_rebuild_orgs(self, cli):
+
+        org = factories.Organization()
+
+        # Update the db directly
+        db_obj = model.Group.get(org["id"])
+        db_obj.title = "Updated title"
+        db_obj.save()
+
+        assert (
+            helpers.call_action("organization_search", q='title:"Updated title"')[
+                "count"
+            ]
+            == 0
+        )
+        result = cli.invoke(ckan, ["sitesearch", "rebuild", "organizations"])
+        assert not result.exit_code
+
+        assert (
+            helpers.call_action("organization_search", q='title:"Updated title"')[
+                "count"
+            ]
+            == 1
+        )
+
+    def test_rebuild_groups(self, cli):
+
+        group = factories.Group()
+
+        # Update the db directly
+        db_obj = model.Group.get(group["id"])
+        db_obj.title = "Updated title"
+        db_obj.save()
+
+        assert (
+            helpers.call_action("group_search", q='title:"Updated title"')["count"] == 0
+        )
+        result = cli.invoke(ckan, ["sitesearch", "rebuild", "groups"])
+        assert not result.exit_code
+
+        assert (
+            helpers.call_action("group_search", q='title:"Updated title"')["count"] == 1
+        )
+
+    def test_rebuild_users(self, cli):
+
+        user = factories.User()
+
+        # Update the db directly
+        db_obj = model.User.get(user["id"])
+        db_obj.fullname = "Updated fullname"
+        db_obj.save()
+
+        assert (
+            helpers.call_action("user_search", q='fullname:"Updated fullname"')["count"]
+            == 0
+        )
+        result = cli.invoke(ckan, ["sitesearch", "rebuild", "users"])
+        assert not result.exit_code
+
+        assert (
+            helpers.call_action("user_search", q='fullname:"Updated fullname"')["count"]
+            == 1
+        )
 
     def test_core_search_index_rebuild_with_r_does_not_clear_the_rest(self, cli):
 
