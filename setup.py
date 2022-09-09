334705de83534f83081f6788c7a235556f5808ad
Patricio Del Boca <patriciodelboca@gmail.com>
2022-09-09 14:06:20 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="""ckanext-sitesearch""",
-    version="0.2.1",
+    version="0.2.2",
     description="""Solr search for other CKAN entities than datasets""",
     long_description=long_description,
     long_description_content_type="text/markdown",
